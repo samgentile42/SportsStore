8a7166b1cbc0edb3b25f71e2a5d1135c4976784a
Sam Gentiile <managedcode44@hotmail.com>
2018-10-09 15:32:59 -0400

    Category, Filter by Categorry, New routes

--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -18,9 +18,10 @@ namespace SportsStore.Controllers
             repository = repo;
         }
 
-        public ViewResult List(int productPage = 1) =>
+        public ViewResult List(string category, int productPage = 1) =>
             View(new ProductsListViewModel {
                 Products = repository.Products
+                        .Where(p => category == null || p.Category == category)
                         .OrderBy(p => p.ProductID)
                         .Skip((productPage - 1) * PageSize)
                 .Take(PageSize),
@@ -29,7 +30,8 @@ namespace SportsStore.Controllers
                     CurrentPage = productPage,
                     ItemsPerPage = PageSize,
                     TotalItems = repository.Products.Count()
-                }
+                },
+                CurrentCategory = category
 
             });
 
--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -37,17 +37,46 @@ namespace SportsStore
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
-                    name: "pagination",
-                    template: "Products/Page{productPage}",
-                    defaults: new { Controller = "Product", action = "List" });
+                    name: null,
+                    template: "{category}/Page{productPage:int}",
+                    defaults: new { controller = "Product", action = "List}" }
+                );
+
+                routes.MapRoute(
+                    name: null,
+                    template: "Page{productPage:int}",
+                    defaults: new
+                    {
+                        controller = "Product",
+                        action = "List",
+                        productPage = 1
+                    }
+                    );
+
+                routes.MapRoute(
+                    name: null,
+                    template: "{category}",
+                    defaults: new
+                    {
+                        controller = "Product",
+                        action = "List",
+                        productPage = 1
+                    }
+                    );
+
+                routes.MapRoute(
+                    name: null,
+                    template: "",
+                    defaults: new
+                    {
+                        controller = "Product",
+                        action = "List",
+                        productPage = 1
+                    });
+
+                routes.MapRoute(name: null, template: "{controller}/{action}/{id?}");
             });
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Product}/{action=List}/{id?}");
-            });
             // SeedData.EnsurePopulated(app);
 
             //app.UseMvc(routes =>
